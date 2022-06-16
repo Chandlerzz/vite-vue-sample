aa8cbc680b06ea8a127263ae4ed831b0d4b7f405
Chandlerzz <Chandlerzz@github.com>
2022-06-16 16:42:43 +0800

    init

---
+++ src/router/index.ts
@@ -0,0 +1,37 @@
+import Vue from "vue";
+import {createRouter,createWebHashHistory} from 'vue-router';
+import Login from "@/views/login.vue"
+import Layout from "@/views/layout.vue"
+const Home = {template:"<div>Home</div>"}
+const About = {template:"<div>About</div>"}
+
+
+const routes = [
+  { path:"/",component:Home },
+  {path:'',component:Home},
+  {
+    path: '/login',
+    name: 'Login',
+    hidden: true,
+    component:Login,
+    meta: {
+      title: '登录'
+    }
+  },
+  {
+    path: '/System',
+    name: 'System',
+    hidden: true,
+    component:Layout,
+    meta: {
+      title: '登录'
+    }
+  },
+  { path:"/about",component:About},
+]
+
+
+ export default createRouter({
+  history:createWebHashHistory(),
+  routes,
+})
